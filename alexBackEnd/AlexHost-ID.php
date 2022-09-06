b7d307da8bbefa459ddafad360bc0c60caa7afbf
silvanuss <103927924+silvanuss@users.noreply.github.com>
2022-09-06 19:54:31 +0700

    Update AlexHost-ID.php

--- alexBackEnd/AlexHost-ID.php
+++ alexBackEnd/AlexHost-ID.php
@@ -6,19 +6,6 @@ header('Location: https://facebook.com/yulio6166');
 die();
 }
 
-if(isset($_GET['playid'])) {
-    $id = $_GET['playid'];
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://alexhost.my.id/true_id/hig/?id=".$id."&token=AlexHost");
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    
-    $res = json_decode($result,true);
-    echo $res['nickname'];
-}
+
 
 ?>
